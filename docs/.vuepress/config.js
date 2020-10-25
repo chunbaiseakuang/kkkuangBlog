d7d66698c05a38222772a49a7212a9b30ebd093b
kkkuang <834778300@qq.com>
2020-10-26 00:06:29 +0800

    feat: 初始化知识库

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,67 @@
+module.exports = {
+  title: 'kkkuang的知识库',
+  description: '呜哇',
+  // 注入到当前页面的 HTML <head> 中的标签
+  head: [
+    ['link', {
+      rel: 'icon',
+      href: '/img/jennie1.ico'
+    }], // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  // base: '/web_accumulate/', // 这是部署到github相关的配置 下面会讲
+  markdown: {
+    lineNumbers: true // 代码块显示行号
+  },
+  themeConfig: {
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+    nav: [{
+        text: '前端算法',
+        link: 'http://obkoro1.com'
+      }, // 内部链接 以docs为根目录
+      {
+        text: '博客',
+        link: 'http://www.baidu.com'
+      }, // 外部链接
+      // 下拉列表
+      {
+        text: 'GitHub',
+        items: [{
+            text: 'GitHub地址',
+            link: 'https://github.com/OBKoro1'
+          },
+          {
+            text: '算法仓库',
+            link: 'https://github.com/OBKoro1/Brush_algorithm'
+          }
+        ]
+      }
+    ],
+
+    sidebar: {
+      // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
+      '/es6/': [
+        {
+          title: '01.ECMAScript 6 简介',
+          collapsable: false,
+          children: [
+            { title: 'items01', path:'/es6/01.ECMAScript 6 简介'}
+            // 上面地址查找的是：docs>accumulate>JS>test.md 文件
+            // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
+          ]
+        }
+      ],
+      // // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
+      // '/algorithm/': [
+      //   '/algorithm/', 
+      //   {
+      //     title: '第二组侧边栏下拉框的标题1',
+      //     children: [
+      //       '/algorithm/simple/test' 
+      //     ]
+      //   }
+      // ]
+    }
+
+  }
+};
